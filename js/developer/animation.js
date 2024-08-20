c4560b0463c99cef31c7ff5712a451d6a9225423
Elenatr07 <e.turkina2010@yandex.ru>
2024-08-20 21:02:08 +1000

    vehicle text

--- js/developer/animation.js
+++ js/developer/animation.js
@@ -1,33 +1,38 @@
-$(window).scroll(function(){
+
+
+let clickOnButton = true;
+
+ function scroling (){
     let height = $(window).scrollTop();
     if(height>500 && height<1500){
-        
         $('#blocks_1').removeClass('hidden_block_1')
-    } else{
-        $('#blocks_1').addClass('hidden_block_1')
+    } else {
+         $('#blocks_1').addClass('hidden_block_1');
     }
-  if(height>1700) {
-    animateSequence();
+    if (height >= 1700 && clickOnButton == true){
+    
+    
     setTimeout(function() {
-       $('.vehicle3').css('display', 'block');
-   
+        $('.vehicle3').css('display', 'block');
+        clickOnButton = false; 
+     
+      }, 1200);
+       setTimeout(function() {
+         $('.vehicle2').css('display', 'block')
+       }, 600);
+       setTimeout(function() {
+         $('.vehicle1').css('display', 'block');
        
-     }, 1200);
-      setTimeout(function() {
-        $('.vehicle2').css('display', 'block')
-      }, 600);
-      setTimeout(function() {
-        $('.vehicle1').css('display', 'block');
-      
-      }, 200);
-          
-      
+       }, 200);
+        animateSequence()
+        
+    } 
+}
 
-  } 
-      function animateSequence() {
+function animateSequence() {
     $('.sequence').each(function() {
         var letter = $(this).text();
-        console.log(letter)
+       // console.log(letter)
         letter = letter.trim();
         var str = '';
         var delay = 1000;
@@ -40,16 +45,16 @@ $(window).scroll(function(){
             }
         }
         $(this).html(str);
+        $('.cssanimation').css('display', 'block')
+        
+        
     });
 }
-  
-     
-    
-})
 
-
-
-
-
-
-
+$(window).scroll(function(){
+	scroling();
+});
+	
+$(document).ready(function(){ 
+	scroling();
+});
