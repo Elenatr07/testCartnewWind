fb216464b4fd8b910c0005349645d7f61b58eb91
Elenatr07 <e.turkina2010@yandex.ru>
2025-04-14 03:51:30 +0300

    Update handler.php

--- php/handler.php
+++ php/handler.php
@@ -12,12 +12,29 @@ $userdata - данные заказчика
 
 
 // Заголовок письма
-$subject = 'Заказ от '.date('d.m.Y').'г.';
+$subject = 'Order from "Amanah trans website" from  <'.$userdata['email_client'].'> ' .date('d.m.Y');
 // ваш Email
 $admin_mail = 'energotr07@yandex.ru';
+$user_mail = $_POST['email_client'];
 // Email заказчика (как fallback - ваш же Email)
 $to = !empty($userdata['email_client']) ? $userdata['email_client'] : $admin_mail;
+foreach($orderlist as $id => $item_data) {
+	
+	$div = '<div>
+		<ul>
+			<li><b>Date:</b> '.$item_data['date'].'</li>
+			<li><b>Time departure:</b> '.$item_data['timedeparture'].'</li>
+			<li><b>Passangers:</b> '.$item_data['pass'].'</li>
+			<li><b>Duration:</b> '.$item_data['hours'].' hours</li>
+			<li><b>Cost:</b> '.$userdata['total_sum'].' IDR</li>
+			<li><b>Pick-up location:</b> '.$userdata['pick_up_location_one_way'].'</li>
+			<li><b>Drop-off location:</b> '.$userdata['drop_off_location_one_way'].'</li>
+			<li><b>Message:</b> '.$userdata['message_ditails'].'</li>
 
+		</ul>
+		</div>
+		';
+}
 
 
 
@@ -28,21 +45,20 @@ $body = '
   <title>'.$subject.'</title>
 </head>
 <body>
-  <p>Информация о заказчике:</p>
+  <p>Customer Information:</p>
 	<ul>
 		<li><b>Client:</b> '.$userdata['full_name_client'].'</li>
 		<li><b>Tel.:</b> '.$userdata['phone_client'].'</li>
 		<li><b>Email:</b> '.$userdata['email_client'].'</li>
-		<li><b>Pick-up location:</b> '.$userdata['pick_up_location_one_way'].'</li>
-		<li><b>Drop-off location:</b> '.$userdata['drop_off_location_one_way'].'</li>
-		<li><b>Cost:</b> '.$userdata['total_sum'].' IDR</li>
-		<li><b>Message:</b> '.$userdata['message_ditails'].'</li>
-		<li><b>Date:</b> '.$item_data['date'].'</li>
+		
+		
+		
+		
 
 	</ul>
-	<p>Информация о заказае:</p>
-
-	<p>Письмо создано автоматически. Пожалуйста, не отвечайте на него, т.к. все ушли на пляж!</p>
+	<p>Order information:</p>
+	'.$div.'
+	<p>The email was created automatically. Please do not answer it.</p>
 </body>
 </html>';
 
@@ -50,22 +66,22 @@ $body = '
 $headers   = []; // или $headers = array() для версии ниже 5.4
 $headers[] = 'MIME-Version: 1.0'; // Обязательный заголовок
 $headers[] = 'Content-type: text/html; charset=utf-8'; // Обязательный заголовок. Кодировку изменить при необходимости
-$headers[] = 'From: Best Shop <noreply@best-shop.piva.net>'; // От кого
+$headers[] = 'From: order cars'; // От кого
 $headers[] = 'Bcc: Admin <'.$admin_mail.'>'; // скрытая копия админу сайта, т.е. вам
 $headers[] = 'X-Mailer: PHP/'.phpversion();
 // Отправка
 $send_ok = mail($to, $subject, $body, implode("\r\n", $headers));
 
 // Ответ на запрос
-$response = [
+/*$response = [
 	'errors' => !$send_ok,
 	'message' => $send_ok ? 'Заказ принят в обработку!' : 'Хьюстон! У нас проблемы!'
-];
+];*/
 // ! Для версий PHP < 5.4 использовать традиционный синтаксис инициализации массивов:
-/*
+
 $response = array (
 	'errors' => !$send_ok,
 	'message' => $send_ok ? 'Заказ принят в обработку!' : 'Хьюстон! У нас проблемы!'
 );
-*/
+
 exit( json_encode($response) );
